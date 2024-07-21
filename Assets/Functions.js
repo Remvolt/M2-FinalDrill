39806276f1658fad39a604e63fc8ac8d92cdd82b
Remvolt <ozzytygon@gmail.com>
2024-07-21 17:33:32 -0400

    Add files via upload

---
+++ Assets/Functions.js
@@ -0,0 +1,31 @@
+function DarkModeToggle(){
+    document.body.style.backgroundColor = "#1f1f1f";
+    document.body.style.color="white";
+    document.getElementById('card1').style.backgroundColor= "#1f1f1f";
+    document.getElementById('card2').style.backgroundColor= "#1f1f1f"; 
+    document.getElementById('card3').style.backgroundColor= "#1f1f1f"; 
+    document.getElementById('card4').style.backgroundColor= "#1f1f1f"; 
+    document.getElementById('card5').style.backgroundColor= "#1f1f1f"; 
+    document.getElementById('card6').style.backgroundColor= "#1f1f1f"; 
+    document.getElementById('card7').style.backgroundColor= "#1f1f1f";
+    document.getElementById('Navbar1').style.backgroundColor= "#1f1f1f";
+    document.getElementById('Navbar2').style.backgroundColor= "#1f1f1f"; 
+    document.getElementById('DarkButton').style.display = "none";
+    document.getElementById('LightButton').style.display = "block";
+}
+
+function LightModeToggle(){
+    document.body.style.backgroundColor = "white";
+    document.body.style.color="black";
+    document.getElementById('card1').style.backgroundColor= "white";
+    document.getElementById('card2').style.backgroundColor= "white"; 
+    document.getElementById('card3').style.backgroundColor= "white"; 
+    document.getElementById('card4').style.backgroundColor= "white"; 
+    document.getElementById('card5').style.backgroundColor= "white"; 
+    document.getElementById('card6').style.backgroundColor= "white"; 
+    document.getElementById('card7').style.backgroundColor= "white";
+    document.getElementById('Navbar1').style.backgroundColor= "white";
+    document.getElementById('Navbar2').style.backgroundColor= "white"; 
+    document.getElementById('DarkButton').style.display = "block";
+    document.getElementById('LightButton').style.display = "none";
+}
